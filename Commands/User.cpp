e6bf32c3c73dc94571c722317991715b3623e4db
vboxuser <vboxuser@pffubuntu.myguest.virtualbox.org>
2025-08-29 19:09:14 +0100

    User fixed, valgrind still need fix

--- Commands/User.cpp
+++ Commands/User.cpp
@@ -7,16 +7,16 @@ void Server::setuser(int fd_c, std::vector<std::string> cmd)
 	std::cout << "Setting up username" << std::endl;
 
 	std::string	realname;
-	std::string	username;
+	const char	*username;
 
 	if (cmd.size() < 5 || cmd[1].size() < 1)
 		return (void)send(fd_c, "ERR_NEEDMOREPARAMS (461)\r\n", 28, 0); 
-	if (cmd[2] != "0" && cmd[2] != "*" || cmd[3] != "0" && cmd[3] != "*") //checar se é suposto ser aspas simples ou duplas
+	if ((cmd[2] != "0" && cmd[2] != "*") || (cmd[3] != "0" && cmd[3] != "*")) //checar se é suposto ser aspas simples ou duplas
 		return (void)send(fd_c, "Wrong arguments, try '<username> (0 *) or (* 0) :<realname>' instead\r\n", 60, 0);
 	if (cmd[1].size() > USERLEN)
 		cmd[1] = (cmd[1].substr(0, USERLEN) + ".");
-	username = cmd[1];
-	if (username[0] == "~")
+	username = cmd[1].c_str();
+	if (username[0] == '~')
 		return (void)send(fd_c, "Username can't start with ~\r\n", 31, 0);
 	for (size_t i = 4; i < cmd.size(); i++)
 		realname = realname + cmd[i];
