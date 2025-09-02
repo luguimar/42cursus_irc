f4890682a1cdc98e66da1c9212d299af171d37f0
Diogo Miguel Teixeira De Sousa <diteixei@c2r5s10.42porto.com>
2025-09-02 16:40:35 +0100

    privmsg fixed and almost done

--- Commands/Privmsg.cpp
+++ Commands/Privmsg.cpp
@@ -25,21 +25,11 @@ void Server::privmsg(int fd_c, std::vector <std::string> cmd)
 	//RPL_AWAY (301)
 	//  "<client> <nick> :<message>"
 	//:localhost <number> <nick> :message
-	//send(_clients[1].getFd(), cmd[2].c_str(), cmd[2].size(), 0);
-	//if its client  look for no client print error ERR_NOSUCHNICK (401) if no server ERR_NOSUCHSERVER (402)
-	//client part (neck nicknames so will not do rn)
 	//if client not online atm send RPL_AWAY (301)
 	//chanel communication (devera fazer)
 	//if channel has some restrictions and cannot send ERR_CANNOTSENDTOCHAN (404)
-	std::string message;
+	std::string message = cmd[2];
 	std::string target = cmd[1];
-
-	for (size_t i = 2; i < cmd.size(); i++)
-	{
-		message += cmd[i];
-		if (i + 1 != cmd.size())
-			message += " ";
-	}
 
 	message += "\r\n";
 
@@ -52,7 +42,8 @@ void Server::privmsg(int fd_c, std::vector <std::string> cmd)
 			send(fd_c, error_msg.c_str(), error_msg.size(), 0);
 			return ;
 		}
-		chan_target->broadcast(message, fd_c);
+		std::string new_message = ":" + client->getNick() + "!" + client->getUser() + "@localhost PRIVMSG " + target + " :" + message;
+		chan_target->broadcast(new_message, fd_c);
 	}
 	else // e um client
 	{
