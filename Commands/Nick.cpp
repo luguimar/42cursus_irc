129cfe8cf37b3fbaef4aef0b97377e2971c4e6ab
Davi Oliveira Silva Siqueira <davioliv@c2r2s3.42porto.com>
2025-09-07 15:23:06 +0100

    nick and user done

--- Commands/Nick.cpp
+++ Commands/Nick.cpp
@@ -7,7 +7,7 @@ int	check_nick(const char *nick)
 	i = -1;
 	while (nick[++i] != '\0')
 	{
-		if (nick[0] == '#' || nick[0] == '&' || nick[0] == ':' || (nick[0] >= '0' && nick[0] <= '9'))
+		if (nick[0] == '#' || nick[0] == '&' || nick[0] == ':' || nick[0] == '*' || (nick[0] >= '0' && nick[0] <= '9'))
 			return (1);
 		else if (nick[i] == ' ')
 			return (1);
@@ -17,42 +17,39 @@ int	check_nick(const char *nick)
 
 void Server::setnick(int fd_c, std::vector<std::string> cmd)
 {
+	std::string	error_msg;
+	Client *client = getClientByFd(fd_c);
 	std::cout << "Setting up nickname" << std::endl;
 
 	if (cmd.size() < 2)
-		return (void)send(fd_c, "ERR_NONICKNAMEGIVEN (431)\r\n", 29, 0);
-	for (size_t i = 0; i < _clients.size(); i++)
 	{
-		if (_clients[i].getNick() == cmd[1])
-			return (void)send(fd_c, "ERR_NICKNAMEINUSE (433)\r\n", 27, 0);
+		error_msg = ":localhost 431 " + client->getNick() + " :No nickname given\r\n";
+		return (void)send(fd_c, error_msg.c_str(), error_msg.size(), 0);
+	}
+	if (client->getNick() == cmd[1])
+	{
+		error_msg = ":localhost 433 " + client->getNick() + " " + cmd[1] + " :Nickname is already in use\r\n";
+		return (void)send(fd_c, error_msg.c_str(), error_msg.size(), 0);
 	}
 	if (check_nick(cmd[1].c_str()))
-		return (void)send(fd_c, "ERR_ERRONEUSNICKNAME (432)\r\n", 30, 0);
+	{
+		error_msg = ":localhost 432 " + client->getNick() + " " + cmd[1] + " :Erroneus nickname\r\n";
+		return (void)send(fd_c, error_msg.c_str(), error_msg.size(), 0);
+	}
 	for (size_t i = 0; i < _clients.size(); i++)
 	{
 		if (_clients[i].getFd() == fd_c)
 		{
-			if (_clients[i].getNick() == "*")
+			if (_clients[i].getNick() != "*")
 			{
-				std::string msg = "Nickname successfully registered as " + cmd[1] + "\r\n";
-				send(fd_c, msg.c_str(), (40 + cmd[1].size()), 0);
-				_clients[i].setNick(cmd[1]);
-			}
-			else
-			{
-				std::string msg = _clients[i].getNick() + "changed his nickname to " + cmd[1] + "\r\n";
-				send(fd_c, msg.c_str(), (_clients[i].getNick().size() + 28 + cmd[i].size()), 0);
+				std::string msg = ":" + _clients[i].getNick() + "!" + _clients[i].getUser() + "@localhost NICK :" + cmd[1] + "\r\n";
+				send(fd_c, msg.c_str(), msg.size(), 0);
 			}
 			_clients[i].setNick(cmd[1]);
-			//:Alejandro!Ale_j@localhost NICK hello2
-			//:oldname!user@localhost NICK newnick
-			//std::string msg2 = ":" + _clients[i].getNick() + "!" + _clients[i].getUser(); + "@localhost " + cmd[0] + " " + cmd[1] + "\r\n";
-			//std::cout << "Msg: " << msg2 << std::endl;
 		}
 	}
 	// falta mandar a mensagem de sucesso pro server
 
-	//esta parte e a tal autenticacao
 	if (getClientByFd(fd_c)->getUser() != "~default" && !getClientByFd(fd_c)->getAuth())
 	{
 		if (getClientByFd(fd_c)->getPass() != _server_pass)
